7881707aad2b94469947b59e77143a604e03af8c
Guilherme Semissatto <semissatto@gmail.com>
2017-02-15 08:45:52 -0200

    Update transformaVetorOffset.py

--- transformaVetorOffset.py
+++ transformaVetorOffset.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python
 
 '''
-#	Código para geração automática de arquivo de configuração de variáveis EPICS 
-#	Execução:
-#	$python (arquivoEntrada.csv) (example.db)
-#	O código também imprime (no terminal) o tamanho total do DataBlock
-#	Guilherme Teixeira Semissatto - LNLS - 07/02/2017 - guilherme.semissatto@lnls.br
+	Comments - SOON
 '''
 
 import sys
